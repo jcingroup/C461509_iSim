3a9959cf680dcb20daa0fcd318b670fb2a75a145
TinWu <yyuuix@gmail.com>
2015-10-28 20:15:15 +0800

    首頁(含行動頁)+廣告+郵寄頁ok

--- Content/js/setting.js
+++ Content/js/setting.js
@@ -1,17 +1,32 @@
 // 回頂端
 $(window).scroll(function () {
     if ($(this).scrollTop() > 100) {
-        $('.goTop').fadeIn();
+        $('.goTop').fadeIn(500);
     } else {
-        $('.goTop').fadeOut();
+        $('.goTop').fadeOut(500);
     }
 });
-$('.goTop').click(function (event) {
+// $('.goTop').click(function (event) {
+//     $('html, body').animate({
+//         scrollTop: $($.attr(this, 'href')).offset().top
+//     }, 1000);
+//     event.preventDefault();
+// });
+
+// // 點選後移到最頂端
+// $('.goTop').click(function(){
+//     $("html, body").animate({ scrollTop: 0 }, 600);
+//     return false;
+// });
+
+// 點選後跳到 href 指向的位置
+$('.scroll-y, .goTop').click(function () {
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 1000);
-    event.preventDefault();
+    return false;
 });
+
 
 // 行動裝置的主選單
 $(".menu-trigger").click(function (event){
@@ -26,11 +41,9 @@ $(".submenu-trigger .btn").click(function (event){
 });
 
 // include
-$(document).ready(function(){
-    $.get("header.html",function(data) {
-        $("#header").html(data);
-    });
-    $.get("footer.html",function(data) {
-        $("#footer").html(data);
-    });
+$.get("_include/header.html",function(data) {
+    $("#header").html(data);
+});
+$.get("_include/footer.html",function(data) {
+    $("#footer").html(data);
 });
