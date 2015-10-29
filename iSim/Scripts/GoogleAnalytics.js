328b18a6fe53e6132906b446452464d37488fc10
Jerry Fu <fusun@ms7.hinet.net>
2015-10-29 23:08:29 +0800

    modify ga

--- iSim/Scripts/GoogleAnalytics.js
+++ iSim/Scripts/GoogleAnalytics.js
@@ -5,5 +5,8 @@
     m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
 })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
-ga('create', 'UA-61909577-5', 'auto');
+ga('create', 'UA-61256035-6', 'auto');
+ga('create', 'UA-61909577-5', { 'name': 'b' });
+
 ga('send', 'pageview');
+ga('b.send', 'pageview');
